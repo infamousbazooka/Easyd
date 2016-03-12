90507b433a51406f9462641f522dec6c795d41f2
infamousbazooka <magnusfernandes1295@gmail.com>
2016-03-12 19:25:49 +0530

    Login designed

--- index.php
+++ index.php
@@ -11,11 +11,26 @@
 </head>
 <body>
 	<div class="mainwrapper">
-		<h1>Hello World!</h1>
-		<p>kdn</p>
+		<section class="head">
+			<div class="logo">
+				<img src="images/logo.png" alt="Logo Here" class="img-responsive">
+			</div>
+		</section>
+		<section class="login">
+			<h1>Login</h1>
+			<form action="login.php">
+				<input class="fill" type="text" required placeholder="Username" name="username" id="username">
+				<input class="fill" type="password" required placeholder="Password" name="password" id="password">
+				<article>
+					<input type="submit" value="SUBMIT">
+					<input type="reset" value="RESET" id="reset">
+				</article>
+			</form>
+		</section>
+		<section class="footer">
+			<p>Footer text here.</p>
+		</section>
 	</div>
-
-
 <script src="js/lib/jquery.min.js"></script>
 <script src="js/main.js"></script>
 </body>
