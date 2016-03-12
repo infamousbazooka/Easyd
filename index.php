e681f06fed955d48f7aa487059ee221144501195
infamousbazooka <magnusfernandes1295@gmail.com>
2016-03-12 18:40:48 +0530

    Initial Commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="icon" href="icon.png" sizes="16x16" type="image/png">
+	<meta name="author" content="Magnus Fernandes - AmberZile">
+	<meta name="description" content="EasyD for Svastek">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<title>EasyD | Home</title>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<script src="js/lib/jquery.min.js"></script>
+<script src="js/main.js"></script>
+</body>
+</html>
